1791086b4d58fc34d7738e1089907a4ccac69730
sjbgit <sjb_strat@hotmail.com>
2024-05-16 00:50:42 +0000

    add to cart component

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+
+const AddToCart = () => {
+  return (
+    <div>
+        <button onClick={()=>console.log('test')}>Add to Cart</button>
+        <div>test add to cart</div>
+    </div>
+   
+  )
+}
+
+export default AddToCart
---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AddToCart from './AddToCart'
+
+const ProductCard = () => {
+  return (
+    <>  
+        <div>Product Card</div>
+        <AddToCart/>
+    </>
+    
+  )
+}
+
+export default ProductCard

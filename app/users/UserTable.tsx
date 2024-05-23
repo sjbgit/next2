da9158cc579789069edfa811d5bb072764126502
sjbgit <sjb_strat@hotmail.com>
2024-05-23 01:31:23 +0000

    sort oder to headers

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -1,3 +1,4 @@
+import Link from '@/node_modules/next/link';
 import React from 'react'
 
 interface User {
@@ -11,17 +12,37 @@ const UserTable = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
     const users: User[] = await res.json();
 
+    const sort = (a: User, b: User) => {
+      const aValue = a.name[0];
+      const bValue = b.name[0]; 
+      if (a < b) {
+        return -1;
+      }
+      else if (a > b) {
+        return 1;
+      }
+      else {
+        return 0; 
+      }
+    }
 
+    const output = () => {
+      console.log(users.sort(sort))
+    }
+
+    output(); 
+    
   return (
     <table className='table table-bordered'>
           <thead>
             <tr>
-              <th>Name (user table separate)</th>
-              <th>Email</th>
+              <th><Link href="/users?sortOrder=name">Name (user table separate)</Link></th>
+              <th><Link href="/users?sortOrder=email">Email</Link></th>
             </tr>
           </thead>
           <tbody>
-            {users.map(user => <tr key={user.id}><td>{user.name}</td><td>{user.email}</td></tr>)}
+          {users.sort(sort).map(user => <tr key={user.id}><td>{user.name}</td><td>{user.email}</td></tr>)}
+            {/* {users.map(user => <tr key={user.id}><td>{user.name}</td><td>{user.email}</td></tr>)} */}
           </tbody>
         </table>
   )

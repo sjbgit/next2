be32c3df98aa53f075bee3710da5ccf87851f606
sjbgit <sjb_strat@hotmail.com>
2024-05-23 01:04:06 +0000

    added sore order param

--- app/products/[[...slug]]/page.tsx
+++ app/products/[[...slug]]/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
 interface Props {
-    params: { slug: string[] }
+    params: { slug: string[] },
+    searchParams: { sortOrder: string }
 }
 
-const ProductPage = ( { params: { slug } }: Props) => {
+const ProductPage = ( { params: { slug }, searchParams: { sortOrder} }: Props) => {
   return (
-    <div>ProductPage {slug} - length: {slug.length}</div>
+    <div>ProductPage {slug} - length: {slug.length} - sort order: {sortOrder}</div>
   )
 }
 

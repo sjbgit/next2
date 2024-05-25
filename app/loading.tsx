90906cd619d58a638fd78428d5e794fa5a735485
sjbgit <sjb_strat@hotmail.com>
2024-05-25 00:15:03 +0000

    added loading to base

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <span className="loading loading-spinner loading-lg"></span>
+    // <div><h1>Loading...</h1></div>
+  )
+}
+
+export default Loading

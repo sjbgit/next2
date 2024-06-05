a4b9fc0c68efebfa1a15e127684fa045bd398f45
sjbgit <sjb_strat@hotmail.com>
2024-06-05 01:20:03 +0000

    api creat user working

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -3,3 +3,11 @@ import { NextRequest, NextResponse } from "@/node_modules/next/server";
 export function GET(request: NextRequest) {
     return NextResponse.json([{id: 1, name: 'mosh'}, { id: 2, name: 'seth'}])
 }
+
+export async function POST(request: NextRequest) {
+    const body = await request.json()
+    if (!body.name) {
+        return NextResponse.json({error: 'name is required'}, {status: 400})
+    }
+    return NextResponse.json({ "id": 1, "name": body.name + ' - test'}, {status: 201})
+}

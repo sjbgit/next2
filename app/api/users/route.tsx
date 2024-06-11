5554f7c723700828ab68e0dd7fca155c53d90a5c
sjbgit <sjb_strat@hotmail.com>
2024-06-11 01:07:47 +0000

    validation for user post

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "@/node_modules/next/server";
+import schema from './schema';
 
 export function GET(request: NextRequest) {
     return NextResponse.json([{id: 1, name: 'mosh'}, { id: 2, name: 'seth'}])
@@ -6,8 +7,14 @@ export function GET(request: NextRequest) {
 
 export async function POST(request: NextRequest) {
     const body = await request.json()
-    if (!body.name) {
-        return NextResponse.json({error: 'name is required'}, {status: 400})
+    const validation = schema.safeParse(body);
+
+    if (!validation.success) {
+        return NextResponse.json({error: validation.error.errors}, {status: 400})
     }
+
+    // if (!body.name) {
+    //     return NextResponse.json({error: 'name is required'}, {status: 400})
+    // }
     return NextResponse.json({ "id": 1, "name": body.name + ' - test POST'}, {status: 201})
 }
